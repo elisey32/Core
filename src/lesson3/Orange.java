96bcabc7ea99d48ce5ea5fb441192ac14166e959
elisey32 <88597617+elisey32@users.noreply.github.com>
2022-11-26 00:08:25 +0300

    Add files via upload

---
+++ src/lesson3/Orange.java
@@ -0,0 +1,13 @@
+package lesson3;
+
+class Orange extends Fruit{            // апельсины
+    public Orange(int count){
+        super(count);
+        this.weight_coeff = 1.5;
+    }
+
+    @Override
+    public String toString() {
+        return "Апельсины {" + count + " штук, массой "+getWeight()+ "f}";
+    }
+}
