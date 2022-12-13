313e8927917c9c82c17908d318333f449f676c6b
elisey32 <88597617+elisey32@users.noreply.github.com>
2022-12-13 14:34:08 +0300

    Add files via upload

---
+++ src/lesson8/WeatherModel.java
@@ -0,0 +1,12 @@
+package lesson8;
+
+import Lesson8.entity.EntityWeather;
+
+import java.io.IOException;
+import java.util.List;
+
+public interface WeatherModel {
+    void getWeather(String selectedCity, Period period) throws IOException;
+
+    List<EntityWeather> getSavedToDBWeather();
+}
